5d1a90197c79258da6851473e8d63d3fa88a4d5c
MBaltz <mateusbaltazar9@gmail.com>
2021-11-10 01:37:18 -0300

    Arquitetura da RIM replicada [gradiente fica 0]

--- RIM.py
+++ RIM.py
@@ -1,4 +1,6 @@
 import tensorflow as tf
+import numpy as np
+from ConvGRU2D import ConvGRU2D
 
 class RNN(tf.keras.Model):
     def __init__(self, in_size):
@@ -6,35 +8,34 @@ class RNN(tf.keras.Model):
         self.in_size = in_size
 
     def build(self, input_shape):
-        self.conv_1 = tf.keras.layers.Conv2D(
-            filters=8, kernel_size=(3,3), strides=(1,1),
-            padding="same", activation="relu", name="conv_1", use_bias=False)
-        self.conv_2 = tf.keras.layers.Conv2D(
-            filters=16, kernel_size=(3,3), strides=(1,1),
-            padding="same", activation="relu", name="conv_2", use_bias=False)
-        self.conv_3 = tf.keras.layers.Conv2D(
-            filters=1, kernel_size=(3,3), strides=(1,1),
-            padding="same", activation="tanh", name="conv_3", use_bias=False) # tanh (pois será somado)
+        self.hidden_1_conv = tf.keras.layers.Conv2D(
+            filters=64, kernel_size=3, strides=2,
+            padding="same", activation="tanh", name="hidden_1_conv")
 
-        self.conv_hidden = tf.keras.layers.Conv2D(
-            filters=1, kernel_size=(3,3), strides=(1,1),
-            padding="same", activation="relu", name="conv_hidden", use_bias=False)
-
-    def call(self, x, grad, hidden, training=True):
-        x_bkp = tf.Variable(x, trainable=False)
-        grad_bkp = tf.Variable(grad, trainable=False)
-        # x = (batch_size, 1000, 513, 2)
-        x = tf.reshape(tf.stack([x, grad]), [self.in_size[0], self.in_size[1], self.in_size[2], 2])
-        out = self.conv_1(x)
-        out = self.conv_2(out)
-
-        new_hidden = self.conv_hidden(
-            tf.reshape(tf.stack([x_bkp, grad_bkp, hidden]), [self.in_size[0], self.in_size[1], self.in_size[2], 3]))
+        self.hidden_2_rnn = ConvGRU2D(
+            filters=16, kernel_size=3, strides=1, padding="same",
+            return_state=False, name="hidden_2_rnn")
         
-        to_att_x = self.conv_3(tf.multiply(out, new_hidden))
+        self.hidden_3_convTranspose = tf.keras.layers.Conv2DTranspose(
+            filters=64, kernel_size=3, strides=2, padding="same",
+            activation="tanh", name="hidden_3_convTranspose")
 
-        # valor que será somado à x e gerando x_t+1 e o novo hidden state
-        return to_att_x, new_hidden
+        self.hidden_4_conv = tf.keras.layers.Conv2D(
+            filters=1, kernel_size=3, strides=1,
+            padding="same", activation="tanh", name="hidden_4_conv")
+
+
+    def call(self, x, grad, training=True):
+        # x = (batch_size, 1000, 513, 2)
+        x = tf.reshape(tf.squeeze(tf.stack([x, grad]), axis=-1), [self.in_size[0], self.in_size[1], self.in_size[2], 2])
+        out = self.hidden_1_conv(x)
+        out = tf.expand_dims(out, axis=1)
+        out = self.hidden_2_rnn(out)
+        out = self.hidden_3_convTranspose(out)
+        out = self.hidden_4_conv(out) # tanh pois será somado
+
+        # valor que será somado à x e gerando x_t+1
+        return out
 
 
 
@@ -47,21 +48,24 @@ class RIM(tf.keras.Model):
         # Dimensão de entrada é (batch_size, x, y)
 
     def build(self, input_shape):
-        self.rnn = RNN(input_shape)
+        self.rnn = RNN(self.in_size)
 
     def call(self, x, training=True):
         # x = tf.expand_dims(x, axis=-1) # (batch_size, 1000, 513, 1)
 
         noised = tf.Variable(x, trainable=False) # Copia x para salvar a imagem ruidosa primária
         # Primeiro hidden state [0 .. 0] com dim=(batch_size, 1000, 513, 1)
-        hidden = tf.Variable(tf.zeros(x.shape), trainable=False)
-        delta_x_noised = self.__gradient_x_noised(noised, x)
+        # hidden = tf.Variable(tf.zeros(x.shape), trainable=False)
 
         for r in range(self.qnt_recurrence):
-            to_att_x, hidden = self.rnn(x, delta_x_noised, hidden, training=True)
-            x = x + to_att_x
-            x = tf.keras.activations.relu(x) # após somar surgem grandes valores
             delta_x_noised = self.__gradient_x_noised(noised, x)
+
+            to_att_x = self.rnn(x, delta_x_noised)
+
+            x = x + to_att_x
+            print(r, np.min(x), np.max(x))
+            # após somar surgem grandes valores ou negativos
+            x = tf.keras.activations.sigmoid(x)
         return x
 
     def __gradient_x_noised(self, noised, x):
--- setup.py
+++ setup.py
@@ -8,6 +8,9 @@ from dival.datasets.standard import get_standard_dataset
 from dival import get_reference_reconstructor
 
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Ignora GPU [RODA APENAS NA CPU]
 os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
 if tf.test.gpu_device_name(): print('GPU found')
 else: print("No GPU found")
@@ -17,42 +20,44 @@ batch_size = 2
 impl = "astra_cpu"
 epocas = 20
 qnt_amostras = 1
-numero_recorrencias = 15
-# mse = tf.keras.losses.MeanSquaredError()
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-
-def loss_mse_manual(target, predicted):
-    return tf.reduce_mean(tf.square(target - predicted))
+numero_recorrencias = 3
+mse = tf.keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.SUM)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
 
 dataset = get_standard_dataset('lodopab', impl=impl)
-print(f"\n\nChecando LoDoPaB-CT: {dataset.check_for_lodopab()}\n")
+# print(f"\n\nChecando LoDoPaB-CT: {dataset.check_for_lodopab()}\n")
 keras_generator = dataset.create_keras_generator("train", batch_size=batch_size, shuffle=False)
 
 rec = get_reference_reconstructor("fbp", "lodopab", impl=impl)
 
-model_rim = RIM(numero_recorrencias, (batch_size, 1000, 513))
+model_rim = RIM(numero_recorrencias, (batch_size, 362, 362))
 
 
 for epoca in range(epocas):
     for i_batch in range(qnt_amostras):
         x, y = keras_generator.__getitem__(i_batch) # [(batch_size, 1000, 513), (batch_size, 362, 362)]
- 
-        # Normaliza entre 0 e 1
-        x -= np.min(x); x /= np.max(x)
-        y -= np.min(x); y /= np.max(x)
-        
         # Reconstrói Sinogramas
         x = np.array([rec.reconstruct(i_x) for i_x in x])
+ 
+        # Normaliza entre 0 e 1
+        x[:,0,0] = 1.0; x[:,0,1] = 0.0
+        x -= np.min(x); x /= np.max(x)
+        x[:,0,0] = 0.0
+        y[:,0,0] = 1.0; y[:,0,1] = 0.0
+        y -= np.min(y); y /= np.max(y)
+        y[:,0,0] = 0.0
+        
         x = tf.expand_dims(x, axis=-1) # (batch_size, 1000, 513, 1)
         y = tf.expand_dims(y, axis=-1) # (batch_size, 1000, 513, 1)
 
         # Salva operações (execução da rede) para derivar e att os pesos
         with tf.GradientTape() as tape:
             prediction = model_rim(x, training=True)
-            loss = loss_mse_manual(y, prediction)
+            loss = mse(y, prediction)
+        print("min max sigmoid", np.min(prediction), np.max(prediction))
 
         for i_img in range(batch_size):
-            print(f"Epoca {epoca},\ti_batch {i_batch}, Amostra {i_img} \tLoss {loss_mse_manual(y[i_img], prediction[i_img]).numpy()}")
+            print(f"Epoca {epoca},\ti_batch {i_batch}, Amostra {i_img} \tLoss {mse(y[i_img], prediction[i_img]).numpy()}")
         print(f"Epoca {epoca},\ti_batch {i_batch}\tLoss {loss.numpy()}\n-----------")
         
         # Calcula gradiente
@@ -60,14 +65,18 @@ for epoca in range(epocas):
         # Atualiza os pesos
         optimizer.apply_gradients(zip(grad, model_rim.trainable_weights))
 
+        # Salva as imagens
         for i_img in range(batch_size):
-            y_cv2 = cv2.normalize(np.float64(y[i_img]), None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
+            y_cv2 = np.squeeze(np.array(y[i_img]*255), axis=-1)
+            # y_cv2 = cv2.normalize(np.float64(y[i_img]), None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
             # cv2.imwrite('gt.png', i_gt)
             
-            x_cv2 = cv2.normalize(np.float64(x[i_img]), None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
+            x_cv2 = np.squeeze(np.array(x[i_img]*255), axis=-1)
+            # x_cv2 = cv2.normalize(np.float64(x[i_img]), None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
             # cv2.imwrite('aux/x_'+str(i)+'.png', x_cv2)
-            
-            precic_rec = cv2.normalize(np.float64(prediction[i_img]), None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
+
+            precic_rec = np.squeeze(np.array(prediction[i_img]*255), axis=-1)
+            # precic_rec = cv2.normalize(np.float64(prediction[i_img]), None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
 
             vis = np.concatenate((y_cv2, x_cv2, precic_rec), axis=1)
             cv2.imwrite("aux/concat_epo"+str(epoca)+"_amostra"+str(i_img)+".png", vis)
