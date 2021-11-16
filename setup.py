9f5c5173fca08cb6fa209e2325d5ea8e76d61a1a
MBaltz <mateusbaltazar9@gmail.com>
2021-11-16 19:18:02 -0300

    Organizando setup e add repos Dataset e ConvGru2D

--- setup.py
+++ setup.py
@@ -1,98 +1,115 @@
 from Network.RIM import RIM
+from Dataset.LoDoPaB-CT_Reconstructor"import TFDataset
 
 import tensorflow as tf
 import numpy as np
-import cv2
+from cv2 import imwrite
+from os import remove, mkdir
+from os.path import join, exists
+from time import time
 
-import glob
-import os
 
-from dival.datasets.standard import get_standard_dataset
-from dival import get_reference_reconstructor
-
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-
-# Ignora GPU [RODA APENAS NA CPU]
-os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
-if tf.test.gpu_device_name(): print('GPU found')
-else: print("No GPU found")
-
-for a in glob.glob("./aux/*.png"):
-    os.remove(a)
-
-batch_size = 1
-seed = 1
-impl = "astra_cpu"
+################################################################################
+id_exec = -1 # -1 se for para uma nova execução. Outro número para retomar.
+dir_execucoes = "/tmp/execucoes/"
+treinar = True
+testar = True
+epoca_inicial = 0 # Se id_exec != -1, alterar época que será carregada
+################################################################################
 epocas = 5
-qnt_amostras = 4 # amostras por época
 numero_recorrencias = 4
-mse = tf.keras.losses.MeanSquaredError()
-# mse = tf.keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.SUM)
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.00007)
+loss_fn = tf.keras.losses.MeanSquaredError()
+# loss_fn = tf.keras.losses.MeanSquaredError(reduction=tf.keras.losses.Reduction.SUM)
+learning_rate = 0.00007
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+seed = 1
+################################################################################
+dir_dataset_npy = "/home/baltz/dados/Dados_2/tcc-database/reco_dataset"
+batch_size = 1
+shuffle = True
+################################################################################
 
 
-dataset = get_standard_dataset('lodopab', impl=impl)
-keras_generator = dataset.create_keras_generator("train", batch_size=batch_size, shuffle=False)
+# Se não for pra treinar ou testar, não tem o que fazer
+if True not in [treinar, testar]:
+    exit(f"\nNada p/ fazer: True not in [treinar, testar]")
 
-rec = get_reference_reconstructor("fbp", "lodopab", impl=impl)
+retomar_exec = False
+if id_exec == -1: id_exec = int(time())
+else: retomar_exec = True
 
-model_rim = RIM(numero_recorrencias, (batch_size, 362, 362, 1))
+# Diretórios importantes
+dir_tfboard = join(dir_execucoes, f"{id_exec}/tfboard/")
+dir_pesos = join(dir_execucoes, f"{id_exec}/pesos/")
+dir_imgs = join(dir_execucoes, f"{id_exec}/imgs/")
 
-if seed != None:
-    tf.random.set_seed(seed)
+# Verifica e constrói os diretorios necessários
+dir_verificar = [dir_execucoes, dir_tfboard, dir_pesos, dir_imgs]
+if not retomar_exec:
+    for d in dir_verificar: mkdir(join(dir_execucoes, d))
+for d in dir_verificar:
+    if not exists(d): exit(f"Diretorio inexistente: {d}")
+
+# Verifica se usará GPU
+if not tf.test.gpu_device_name(): print('No ', end=""); print('GPU found\n\n')
+
+# Utiliza a seed (caso tenha) antes de instanciar qualquer coisa randomica
+if seed != None: tf.random.set_seed(seed)
+
+# Carrega todos os Datasets
+train_dataset = TFDataset(dir_dataset_npy, "train", batch_size,
+    shuffle=shuffle, return_with_channel=True)
+test_dataset = TFDataset(dir_dataset_npy, "test", batch_size,
+    shuffle=shuffle, return_with_channel=True)
+vali_dataset = TFDataset(dir_dataset_npy, "validation", batch_size,
+    shuffle=shuffle, return_with_channel=True)
 
 
-for epoca in range(epocas):
+model = RIM(numero_recorrencias, (batch_size, 362, 362, 1))
+
+
+def treinar(epoca):
     list_loss = []
-    for i_amostra in range(qnt_amostras):
-        x, y = keras_generator.__getitem__(i_amostra) # [(batch_size, 1000, 513), (batch_size, 362, 362)]
-        # Reconstrói Sinogramas
-        x = np.array([rec.reconstruct(i_x) for i_x in x])
- 
-        # Normaliza entre 0 e 1
-        for i_batch in range(batch_size):
-            x[i_batch] -= np.min(x[i_batch]); x[i_batch] /= np.max(x[i_batch])
-            x[i_batch] *= np.max(y[i_batch])
-        
-        x = tf.expand_dims(x, axis=-1) # (batch_size, 1000, 513, 1)
-        y = tf.expand_dims(y, axis=-1) # (batch_size, 1000, 513, 1)
-
-        # Salva operações (execução da rede) para derivar e att os pesos
+    count_batch = 0
+    for x, y in train_dataset: # x/y.shape: (batch_size, 1000, 513, 1)
+        # Salva operações (execução da rede) para calcular o gradiente
         with tf.GradientTape() as tape:
-            prediction = model_rim(x, training=True)
-            loss = mse(y, prediction)
-        print("min max predicao", np.min(prediction), np.max(prediction))
+            prediction = model(x, training=True)
+            loss = loss_fn(y, prediction)
 
-        for i_batch in range(batch_size):
-            print(f"Epoca {epoca},\ti_amostra {i_amostra}, i_batch {i_batch} \tLoss {mse(y[i_batch], prediction[i_batch]).numpy()}")
-        print(f"Epoca {epoca},\ti_amostra {i_amostra}\tLoss {loss.numpy()}\n-----------")
-        
-        # Calcula gradiente
-        grad = tape.gradient(loss, model_rim.trainable_weights)
-        # Atualiza os pesos
-        optimizer.apply_gradients(zip(grad, model_rim.trainable_weights))
-
-
-        # Salva as imagens
-        for i_batch in range(batch_size):
-            y_cv2 = np.squeeze(np.array(y[i_batch]*255), axis=-1)
-            # y_cv2 = cv2.normalize(np.float64(y[i_batch]), None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
-            # cv2.imwrite('gt.png', i_gt)
-            
-            x_cv2 = np.squeeze(np.array(x[i_batch]*255), axis=-1)
-            # x_cv2 = cv2.normalize(np.float64(x[i_batch]), None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
-            # cv2.imwrite('aux/x_'+str(i)+'.png', x_cv2)
-
-            precic_rec = np.squeeze(np.array(prediction[i_batch]*255), axis=-1)
-            # precic_rec = cv2.normalize(np.float64(prediction[i_batch]), None, alpha = 0, beta = 255, norm_type = cv2.NORM_MINMAX, dtype = cv2.CV_32F)
-
-            vis = np.concatenate((y_cv2, x_cv2, precic_rec), axis=1)
-            cv2.imwrite("aux/concat_epo"+str(epoca)+"_amostra"+str(i_amostra)+"_batch"+str(i_batch)+".png", vis)
-
-
+        print(f"Epoca {epoca}, \tcount_batch {count_batch}, \tLoss {loss.numpy()}")
         list_loss.append(loss.numpy())
 
-    print(f"Média loss epoca {epoca}: {np.sum(list_loss)/len(list_loss)}")
+        # Calcula gradiente
+        grad = tape.gradient(loss, model.trainable_weights)
+        # Atualiza os pesos
+        optimizer.apply_gradients(zip(grad, model.trainable_weights))
+
+        # Salva as imagens
+        for i_amostra in range(len(x)):
+            y_cv2 = np.squeeze(np.array(y[i_amostra]*255), axis=-1)
+            x_cv2 = np.squeeze(np.array(x[i_amostra]*255), axis=-1)
+            precic_cv2 = np.squeeze(np.array(prediction[i_amostra]*255), axis=-1)
+
+            vis = np.concatenate((y_cv2, x_cv2, precic_cv2), axis=1)
+            nome_img = f"concat_epo"{epoca}"_batch"{count_batch}"_amostra"{i_amostra}".png"
+            imwrite(join(dir_imgs, nome_img), vis)
+
+    print(f"Média do loss da epoca {epoca}: {np.sum(list_loss)/len(list_loss)}")
+
+
+def testar(epoca):
+    raise NotImplementedError
+
+# model.save_weights(dir_pesos, overwrite=True)
+# exit()
+
+for epoca in range(epocas):
+    if treinar:
+        treinar(epoca)
+    if testar:
+        testar(epoca)
+    
 
 # seed = 1: Média loss epoca 4: 0.0008389844442717731
 # seed = 1: Média loss epoca 4: 0.0008389844442717731
